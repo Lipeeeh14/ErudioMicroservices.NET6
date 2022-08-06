c4694be3d75895f008e81b4cb786d84b9c9b44b7
Luiz Felipe <lipeeeh14@gmail.com>
2022-08-06 14:43:13 -0300

    FEAT[Product]: Configuração do banco e criação da migration

---
+++ GeekShooping/GeekShooping.ProductAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.ProductAPI.Model.Context
+{
+	public class SqlServerContext : DbContext
+	{
+		public SqlServerContext() {}
+
+		public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) {}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
--- GeekShooping/GeekShooping.ProductAPI/Program.cs
+++ GeekShooping/GeekShooping.ProductAPI/Program.cs
@@ -1,13 +1,14 @@
+using GeekShooping.ProductAPI.Model.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSqlServer<SqlServerContext>(builder.Configuration["ConnectionString:GeekShooping_ProductAPI"]);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
